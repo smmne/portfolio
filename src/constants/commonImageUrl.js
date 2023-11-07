a1b00ed6c7f9504c0f69f10648cbedb82176b384
smmne <kdaumh33@gmail.com>
2023-11-07 19:58:42 +0900

    img 경로 변경

--- src/constants/commonImageUrl.js
+++ src/constants/commonImageUrl.js
@@ -1,4 +1,4 @@
-export const MAIN_BANNER_URL = "/static/images/main_banner.jpg";
+export const MAIN_BANNER_URL = "./static/images/main_banner.jpg";
 export const ICON_HTML_URL = "/static/images/icons/icon_html.png";
 export const ICON_CSS_URL = "/static/images/icons/icon_css.png";
 export const ICON_JAVASCRIPT_URL = "/static/images/icons/icon_javascript.png";
